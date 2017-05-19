7f1587a26b69672a45ba91f177da2b9918140060
Shane Liesegang <shane@techie.net>
2017-05-19 07:58:34 -0700

    preference panel for screensaver

---
+++ LifeSaver/LifeSaverPreferencesController.swift
@@ -0,0 +1,81 @@
+import Cocoa
+import ScreenSaver
+import simd
+
+class LifeSaverPreferencesController: NSWindowController {
+    let defaults: ScreenSaverDefaults? = ScreenSaverDefaults(forModuleWithName: Bundle(for: LifeSaverPreferencesController.self).bundleIdentifier!)
+    
+    @IBOutlet weak var bgColorWell: NSColorWell!
+    @IBOutlet weak var fgColorWell: NSColorWell!
+    @IBOutlet weak var pointSizeSlider: NSSlider! {
+        didSet {
+            pointSizeSlider.minValue = 1.0
+            pointSizeSlider.maxValue = 13.0
+            pointSizeSlider.doubleValue = 9.0
+        }
+    }
+    @IBOutlet weak var pointSizeLabel: NSTextField!
+    
+    @IBAction func okButtonAction(_ sender: Any) {
+        self.defaults?.set(pointSizeSlider.doubleValue, forKey: "pointSize")
+        self.defaults?.set(bgColorWell.color.redComponent, forKey: "bgRed")
+        self.defaults?.set(bgColorWell.color.greenComponent, forKey: "bgGreen")
+        self.defaults?.set(bgColorWell.color.blueComponent, forKey: "bgBlue")
+        self.defaults?.set(fgColorWell.color.redComponent, forKey: "fgRed")
+        self.defaults?.set(fgColorWell.color.greenComponent, forKey: "fgGreen")
+        self.defaults?.set(fgColorWell.color.blueComponent, forKey: "fgBlue")
+        
+        self.defaults?.synchronize()
+        NSApp.mainWindow?.endSheet(self.window!)
+    }
+    @IBAction func restoreButtonAction(_ sender: Any) {
+        self.defaults?.set(9.0, forKey: "pointSize")
+        self.defaults?.set(0.25, forKey: "bgRed")
+        self.defaults?.set(0.0, forKey: "bgGreen")
+        self.defaults?.set(0.0, forKey: "bgBlue")
+        self.defaults?.set(1.0, forKey: "fgRed")
+        self.defaults?.set(0.0, forKey: "fgGreen")
+        self.defaults?.set(0.0, forKey: "fgBlue")
+        
+        self.syncUI()
+    }
+    @IBAction func sliderValueChanged(_ sender: Any) {
+        self.pointSizeLabel.stringValue = String(format: "%.1f", arguments: [self.pointSizeSlider.doubleValue])
+    }
+    
+    required init?(coder decoder: NSCoder) {
+        super.init(coder: decoder)
+    }
+    override init(window: NSWindow?) {
+        super.init(window: window)
+    }
+    
+    override func windowDidLoad() {
+        super.windowDidLoad()
+        
+        let prefsFilePath = Bundle(for: LifeSaverPreferencesController.self).path(forResource: "InitialPreferences", ofType: "plist")
+        let nsDefaultPrefs = NSDictionary(contentsOfFile: prefsFilePath!)
+        if let defaultPrefs: Dictionary<String,Any> = nsDefaultPrefs as? Dictionary<String, Any> {
+            self.defaults?.register(defaults: defaultPrefs)
+        }
+        
+        self.syncUI()
+    }
+    
+    func syncUI() {
+        self.pointSizeSlider.doubleValue = self.defaults!.double(forKey: "pointSize")
+        self.pointSizeLabel.stringValue = String(format: "%.1f", arguments: [self.pointSizeSlider.doubleValue])
+        self.bgColorWell.color = NSColor(
+            red: CGFloat(self.defaults!.double(forKey: "bgRed")),
+            green: CGFloat(self.defaults!.double(forKey: "bgGreen")),
+            blue: CGFloat(self.defaults!.double(forKey: "bgBlue")),
+            alpha: 1.0
+        )
+        self.fgColorWell.color = NSColor(
+            red: CGFloat(self.defaults!.double(forKey: "fgRed")),
+            green: CGFloat(self.defaults!.double(forKey: "fgGreen")),
+            blue: CGFloat(self.defaults!.double(forKey: "fgBlue")),
+            alpha: 1.0
+        )
+    }
+}
--- LifeSaver/LifeSaverView.swift
+++ LifeSaver/LifeSaverView.swift
@@ -5,10 +5,12 @@ import MetalKit
 class LifeSaverView: ScreenSaverView {
     var lifeView: LifeView?
     var eqTimer: Timer?
+    var prefsWindowController: LifeSaverPreferencesController?
+    let defaults: ScreenSaverDefaults? = ScreenSaverDefaults(forModuleWithName: Bundle(for: LifeSaverPreferencesController.self).bundleIdentifier!)
     
     override init?(frame: NSRect, isPreview: Bool) {
         super.init(frame: frame, isPreview: isPreview)
-                
+        
         if let device = MTLCreateSystemDefaultDevice() {
             self.lifeView = LifeView(frame: frame, device: device)
         }
@@ -18,7 +20,7 @@ class LifeSaverView: ScreenSaverView {
 
         self.lifeView!.delegate = self
         self.lifeView!.preferredFramesPerSecond = 60
-        self.lifeView!.clearColor = MTLClearColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
+        self.lifeView!.clearColor = MTLClearColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         
         self.addSubview(lifeView!)
     }
@@ -26,11 +28,28 @@ class LifeSaverView: ScreenSaverView {
     override func viewDidMoveToWindow() {
         super.viewDidMoveToWindow()
         
+        if self.window == nil {
+            return
+        }
+        
         if let device = MTLCreateSystemDefaultDevice() {
             lifeView!.setup(device: device)
             
-            lifeView!.uniforms.bgColor = float4(0.25, 0.0, 0.0, 1.0)
-            lifeView!.uniforms.fgColor = float4(1.0, 0.0, 0.0, 1.0)
+            if (self.defaults != nil) {
+                lifeView!.uniforms.pointSize = self.defaults!.float(forKey: "pointSize")
+                lifeView!.uniforms.bgColor = float4(
+                    self.defaults!.float(forKey: "bgRed"),
+                    self.defaults!.float(forKey: "bgGreen"),
+                    self.defaults!.float(forKey: "bgBlue"),
+                    1.0
+                )
+                lifeView!.uniforms.fgColor = float4(
+                    self.defaults!.float(forKey: "fgRed"),
+                    self.defaults!.float(forKey: "fgGreen"),
+                    self.defaults!.float(forKey: "fgBlue"),
+                    1.0
+                )
+            }
             
             eqTimer = Timer.scheduledTimer(timeInterval: 3.0, target: self.lifeView!, selector: #selector(LifeView.checkForEquilibrium), userInfo: nil, repeats: true)
         }
@@ -41,6 +60,18 @@ class LifeSaverView: ScreenSaverView {
         fatalError("init(coder:) has not been implemented")
     }
     
+    override func hasConfigureSheet() -> Bool {
+        return true
+    }
+    
+    override func configureSheet() -> NSWindow? {
+        if let controller = self.prefsWindowController {
+            return controller.window
+        }
+        
+        self.prefsWindowController = LifeSaverPreferencesController(windowNibName: "LifeSaverPreferences")
+        return self.prefsWindowController!.window
+    }
 }
 
 extension LifeSaverView: MTKViewDelegate {
